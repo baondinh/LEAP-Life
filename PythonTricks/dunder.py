4bb9ba3453d11abeb3e124b049aba9b0cf7a6b29
Bao Dinh <49595165+baondinh@users.noreply.github.com>
2025-04-07 13:40:53 -0400

    init dunder example

--- PythonTricks/dunder.py
+++ PythonTricks/dunder.py
@@ -1,1 +1,10 @@
 # Review of dunder (double underscore) Python methods
+# Special reserved Python methods that map to somke sort of behavior
+
+# Define behavior when a new instance of an object is created (OOP)
+class Rect: 
+  def __init__(self, x, y): 
+    self.x = x
+    self.y = y
+
+Rect(2,3)
