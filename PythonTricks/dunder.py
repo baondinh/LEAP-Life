3f6d8fcb932fa10797dcd08452afbf453c11ba46
Bao Dinh <49595165+baondinh@users.noreply.github.com>
2025-04-07 13:47:34 -0400

    add dunder example and explanation

--- PythonTricks/dunder.py
+++ PythonTricks/dunder.py
@@ -1,10 +1,24 @@
 # Review of dunder (double underscore) Python methods
+# Credit: Tech with Time https://www.youtube.com/watch?v=qqp6QN20CpE
 # Special reserved Python methods that map to somke sort of behavior
+# Everything created in Python is really an object
+def func():
+  pass
+print(type(func))
 
-# Define behavior when a new instance of an object is created (OOP)
+# __init__ Define behavior when a new instance of an object is created (OOP)
 class Rect: 
   def __init__(self, x, y): 
     self.x = x
     self.y = y
-
 Rect(2,3)
+
+# __add__ Defines behavior when two objects of same type added together
+str1 = "hello"
+str2 = "world"
+new_str = str1 + str2 
+# We take concatenation for granted, but how does Python know to add two string objects?
+# There is an __add__ method defined in the String class
+new_str = str1.__add__(str2) # Can call the function directly instead of using + operator 
+print(new_str)
+
